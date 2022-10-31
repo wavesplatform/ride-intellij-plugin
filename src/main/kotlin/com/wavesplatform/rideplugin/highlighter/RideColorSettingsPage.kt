edfdfd41df84235d18e2ff7335ecac55dea5a2fb
Alexandr Devyatkin <adevyatkin@uniqsoft.ae>
2022-10-31 11:08:25 +0300

    - fix highlight

--- src/main/kotlin/com/wavesplatform/rideplugin/highlighter/RideColorSettingsPage.kt
+++ src/main/kotlin/com/wavesplatform/rideplugin/highlighter/RideColorSettingsPage.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.options.colors.AttributesDescriptor
 import com.intellij.openapi.options.colors.ColorDescriptor
 import com.intellij.openapi.options.colors.ColorSettingsPage
 import com.wavesplatform.rideplugin.RideIcons
-import java.util.regex.Pattern
 import javax.swing.Icon
 
 
@@ -20,68 +19,145 @@ class RideColorSettingsPage : ColorSettingsPage {
     }
 
     override fun getDemoText(): String {
-        return """{-# STDLIB_VERSION 3 #-}
-        {-# CONTENT_TYPE DAPP #-}
-        {-# SCRIPT_TYPE ACCOUNT #-}
-        
-        #
-        # Waves dApp version of famous Magic 8 ball toy!
-        # Function name: tellme(question: String)
-        # Question and answer will be written to dApp data state
-        #
-        # Example on Testnet: 3N27HUMt4ddx2X7foQwZRmpFzg5PSzLrUgU
-        #
-        
-        let answersCount = 20
-        let answers = 
-            ["It is certain.",
-            "It is decidedly so.",
-            "Without a doubt.",
-            "Yes - definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful."]
-        
-        
-        #
-        # Simple pseudorandom answer generator
-        #
-        func getAnswer(question: String, previousAnswer: String) = {
-            let hash = sha256(toBytes(question + previousAnswer))
-            let index = toInt(hash)
-            #answers[index % answersCount]
-        }
-        
-        func getPreviousAnswer(address: String) = {
-          match getString(this, address + "_a") {
-            case a: String => a
-            case _ => address
-          }
-        }
-        
+        return """
+        {-# SCRIPT_TYPE  ACCOUNT #-}
+        {-# CONTENT_TYPE LIBRARY #-}
+        {-# STDLIB_VERSION 5 #-}
+
+        # hm
+        # another
+        let a = 42                 # Integer variable definition
+        let b = "Ride the Waves!"  # String variable definition
+        let bool = true  # Boolean variable definition
+
         @Callable(i)
-        func tellme(question: String) = {
-            let callerAddress = toBase58String(i.caller.bytes)
-            let answer = getAnswer(question, getPreviousAnswer(callerAddress))
-        
-            WriteSet([
-                DataEntry(callerAddress + "_q", question),
-                DataEntry(callerAddress + "_a", answer)
-                ])
-        }""".trimIndent()
+        func lazyIsGood() = {
+          let a = base58'AAAAA'
+          strict balanceBefore = wavesBalance(this).value().regular
+          strict result = invoke(
+            dApp2,
+            "bar",
+            args,
+            [AttachedPayment(unit,100000000)]
+          )
+          strict balanceAfter = wavesBalance(this).regular
+          let c = {
+             strict d = 1
+             true
+            }
+          c
+        }
+
+        func reward() = {
+           let p = value(i.payments[0])
+           if p.assetId != lpTokenId {
+            true
+           } else {
+            false
+           }
+           if p.assetId > lpTokenId
+              then throw("Incorrect payment")
+              else {
+                 let lpQty = assetInfo(lpTokenId).value().quantity
+                 let rewardQty = assetInfo(rewardTokenId).value().quantity
+                 let share = fraction(p.amount,rewardQty,lpQty)
+                 strict result = invoke(this, "burn", [p.amount], [1])
+                 [
+                    ScriptTransfer(i.caller, share, rewardTokenId)
+                 ]
+              }
+        }
+
+        let list = [1, 2, 3]
+        let a1 = list.size()
+        let a2 = size(list)
+
+        let b1 = getInteger(this, "key")
+        let b2 = this.getInteger("key")
+
+        func say() = {
+          "Hello world!"
+        }
+
+        func main(amount: Int) = {
+           if (amount > 0)
+           then
+                "Done!"
+           else
+                throw("Error!")
+        }
+
+        let someConstant = 42
+        func doSomething() = {
+            1 + 1
+        }
+
+        func main(amount: Int, assetId: ByteVector|Unit, names: List[String]) = {
+           throw("Error")
+        }
+
+        match tx {
+            case ex: ExchangeTransaction|MassTransferTransaction =>
+                let pair = AssetPair(assetId, unit)
+                let rate = extract(getInteger(issuer, "exchangeRate"))
+                let rateDiff = rate - fraction(100000000, ex.price, ex.amount)
+                ex.buyOrder.assetPair == pair && rateDiff > -100 && rateDiff < 100
+            case _ => {true}
+        }
+
+        match tx {
+            case t: TransferTransaction|ExchangeTransaction|MassTransferTransaction|Order => false   # deny any transfer of funds from the account
+            case _: Transaction => sigVerify(tx.bodyBytes, tx.proofs[0], tx.senderPublicKey) # allow all other known transaction types if the signature is correct
+            case _ => {false}  # deny any other (new, unknown) entity type since they are not supported in the Ride version used
+        }
+
+        func sum(accum: Int, next: Int) = accum
+        let arr = [1,2,3,4,5]
+        FOLD<5>(arr, 0, sum)    # Result: 15
+
+        func mult(accum: Int, next: Int) = { accum }
+        let arr = [
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+        FOLD<5>(arr, 1, mult)    # Result: 120
+
+        func filterEven(accum: List[Int|String], next: Int) = {
+           if (next % 2 == 0) then accum :+ next else accum
+        }
+
+        let arr = [1,2,3,4,5]
+        FOLD<5>(arr, [], filterEven)    # Result: [2, 4]
+
+        func map(accum: List[Int], next: Int) = (next - 1) :: accum
+        let arr = [1, 2, 3, 4, 5]
+        FOLD<5>(arr, [], map)    # Result: [4, 3, 2, 1, 0]
+
+        let keys = ["key1", "key2", "key3"]
+        let values = ["value1", "value2", "value3"]
+
+        func addStringEntry(accum: (List[StringEntry], Int), nextValue: String) =
+           {    #comment
+              let (result, j) = accum
+              (result :+ StringEntry(keys[j], nextValue), j + 1)
+           }
+        let r = FOLD<10>(values, ([], 0), addStringEntry)
+        r._1
+
+        wavesBalance(Address(base58'3Mz9N7YPfZPWGd4yYaX6H53Gcgrq6ifYiH7'))
+        wavesBalance(Alias("merry"))                # The result is the same
+
+        let addr = Address(base58'3N4iKL6ikwxiL7yNvWQmw7rg3wGna8uL6LU')
+        getInteger(addr,"integerVal")               # Returns 1
+        getInteger(addr,"noKey")                    # Returns unit
+
+        size(getString("key"))
+
+        size(getStringValue("key"))
+        """.trimIndent()
     }
 
     override fun getAdditionalHighlightingTagToDescriptorMap(): Map<String, TextAttributesKey>? {
@@ -102,12 +178,15 @@ class RideColorSettingsPage : ColorSettingsPage {
 
     companion object {
         private val DESCRIPTORS = arrayOf(
-            AttributesDescriptor("Key", RideSyntaxHighlighter.KEY),
-            AttributesDescriptor("Separator", RideSyntaxHighlighter.SEPARATOR),
-            //AttributesDescriptor("Value", RideSyntaxHighlighter.VALUE),
+            AttributesDescriptor("Keyword", RideSyntaxHighlighter.KEYWORDS),
+            AttributesDescriptor("Number", RideSyntaxHighlighter.NUMBER),
+            AttributesDescriptor("String", RideSyntaxHighlighter.STRING),
+            AttributesDescriptor("Brackets", RideSyntaxHighlighter.BRACKETS),
+            AttributesDescriptor("Operator", RideSyntaxHighlighter.OPERATORS),
             AttributesDescriptor("Bad value", RideSyntaxHighlighter.BAD_CHARACTER),
+            AttributesDescriptor("Identifier", RideSyntaxHighlighter.IDENTIFIER),
             AttributesDescriptor("Directive", RideSyntaxHighlighter.DIRECTIVE),
-            AttributesDescriptor("Annotation", RideSyntaxHighlighter.ANNOTATION)
+            AttributesDescriptor("Annotation", RideSyntaxHighlighter.ANNOTATION),
         )
     }
 }
--- src/main/kotlin/com/wavesplatform/rideplugin/highlighter/RideSyntaxHighlighter.kt
+++ src/main/kotlin/com/wavesplatform/rideplugin/highlighter/RideSyntaxHighlighter.kt
@@ -5,51 +5,59 @@ import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.HighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
-import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import com.wavesplatform.rideplugin.RideLexerAdapter
-import com.wavesplatform.rideplugin.psi.RideTypes
+import com.wavesplatform.rideplugin.parser.RideParserDefinition
 
 
 class RideSyntaxHighlighter : SyntaxHighlighterBase() {
+
+    private val attributes: Map<IElementType, TextAttributesKey> = HashMap()
+
+    companion object {
+        val KEYWORDS = TextAttributesKey.createTextAttributesKey("RIDE_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD)
+        val NUMBER = TextAttributesKey.createTextAttributesKey("RIDE_NUMBER", DefaultLanguageHighlighterColors.NUMBER)
+        val STRING = TextAttributesKey.createTextAttributesKey("RIDE_STRING", DefaultLanguageHighlighterColors.STRING)
+        val OPERATORS = TextAttributesKey.createTextAttributesKey("RIDE_OPERATORS", DefaultLanguageHighlighterColors.OPERATION_SIGN)
+        val IDENTIFIER = TextAttributesKey.createTextAttributesKey("RIDE_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER)
+        val BAD_CHARACTER =
+            TextAttributesKey.createTextAttributesKey("RIDE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
+        val COMMA =
+            TextAttributesKey.createTextAttributesKey("RIDE_COMMA", DefaultLanguageHighlighterColors.COMMA)
+        val COLON = TextAttributesKey.createTextAttributesKey("RIDE_COLON", HighlighterColors.TEXT)
+
+        val BRACKETS = TextAttributesKey.createTextAttributesKey("RIDE_BRACKETS", DefaultLanguageHighlighterColors.BRACKETS)
+        val PARENTHESES = TextAttributesKey.createTextAttributesKey("RIDE_PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES)
+        val BRACES = TextAttributesKey.createTextAttributesKey("RIDE_BRACES", DefaultLanguageHighlighterColors.BRACES)
+        val COMMENT =
+            TextAttributesKey.createTextAttributesKey("RIDE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+        val DIRECTIVE =
+            TextAttributesKey.createTextAttributesKey("RIDE_DIRECTIVE", DefaultLanguageHighlighterColors.METADATA)
+        val ANNOTATION =
+            TextAttributesKey.createTextAttributesKey("RIDE_ANNOTATION", DefaultLanguageHighlighterColors.LABEL)
+    }
+
+    init {
+        fillMap(attributes, RideParserDefinition.DIRECTIVE, DIRECTIVE)
+        fillMap(attributes, RideParserDefinition.ANNOTATION, ANNOTATION)
+        fillMap(attributes, RideParserDefinition.KEYWORDS, KEYWORDS)
+        fillMap(attributes, RideParserDefinition.NUMBERS, NUMBER)
+        fillMap(attributes, RideParserDefinition.STRINGS, STRING)
+        fillMap(attributes, RideParserDefinition.OPERATORS, OPERATORS)
+        fillMap(attributes, RideParserDefinition.IDENTIFIER, IDENTIFIER)
+        fillMap(attributes, RideParserDefinition.COMMA, COMMA)
+        fillMap(attributes, RideParserDefinition.PARENTHESES, PARENTHESES)
+        fillMap(attributes, RideParserDefinition.COLON, COLON)
+        fillMap(attributes, RideParserDefinition.BRACKETS, BRACKETS)
+        fillMap(attributes, RideParserDefinition.BRACES, BRACES)
+        fillMap(attributes, RideParserDefinition.COMMENT, COMMENT)
+    }
+
     override fun getHighlightingLexer(): Lexer {
         return RideLexerAdapter()
     }
 
     override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> {
-        return when (tokenType) {
-            RideTypes.ASSIGN -> SEPARATOR_KEYS
-            RideTypes.DIRECTIVE -> DIRECTIVE_KEYS
-            RideTypes.IDENT -> VALUE_KEYS
-            RideTypes.COMMENT -> COMMENT_KEYS
-            RideTypes.ANNOTATION -> ANNOTATION_KEYS
-            TokenType.BAD_CHARACTER -> BAD_CHAR_KEYS
-            else -> EMPTY_KEYS
-        }
-    }
-
-    companion object {
-        val SEPARATOR = TextAttributesKey.createTextAttributesKey(
-            "SIMPLE_SEPARATOR",
-            DefaultLanguageHighlighterColors.OPERATION_SIGN
-        )
-        val KEY = TextAttributesKey.createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD)
-        val VALUE = TextAttributesKey.createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING)
-        val COMMENT =
-            TextAttributesKey.createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
-        val BAD_CHARACTER =
-            TextAttributesKey.createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
-        val DIRECTIVE =
-            TextAttributesKey.createTextAttributesKey("SIMPLE_DIRECTIVE", DefaultLanguageHighlighterColors.METADATA)
-        val DIRECTIVE_KEYS = arrayOf(DIRECTIVE)
-        val ANNOTATION =
-            TextAttributesKey.createTextAttributesKey("SIMPLE_ANNOTATION", DefaultLanguageHighlighterColors.METADATA)
-        val ANNOTATION_KEYS = arrayOf(ANNOTATION)
-        private val BAD_CHAR_KEYS = arrayOf(BAD_CHARACTER)
-        private val SEPARATOR_KEYS = arrayOf(SEPARATOR)
-        private val KEY_KEYS = arrayOf(KEY)
-        private val VALUE_KEYS = arrayOf(VALUE)
-        private val COMMENT_KEYS = arrayOf(COMMENT)
-        private val EMPTY_KEYS = arrayOf<TextAttributesKey>()
+        return pack(attributes[tokenType])
     }
 }
