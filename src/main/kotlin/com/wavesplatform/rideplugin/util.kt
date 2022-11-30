4df760f1a82cf82708750b4b0c7e811112101eed
Alexandr Devyatkin <adevyatkin@uniqsoft.ae>
2022-11-30 19:45:07 +0300

    Color settings page fix
     - add highlight in color settings page for directive, function and type

--- src/main/kotlin/com/wavesplatform/rideplugin/util.kt
+++ src/main/kotlin/com/wavesplatform/rideplugin/util.kt
@@ -6,7 +6,5 @@ import java.io.InputStreamReader
 
 fun loadCodeSampleResource(ctx: Any, resource: String): String {
     val stream = ctx.javaClass.classLoader.getResourceAsStream(resource)!!
-    // We need to convert line separators here, because IntelliJ always expects \n,
-    // while on Windows the resource file will be read with \r\n as line separator.
     return StreamUtil.convertSeparators(StreamUtil.readText(BufferedReader(InputStreamReader(stream))))
 }
