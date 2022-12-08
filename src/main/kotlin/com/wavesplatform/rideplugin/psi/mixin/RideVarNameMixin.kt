147e1fc1aa261ae9a1040aa2425ea78f9311927d
Alexandr Devyatkin <adevyatkin@uniqsoft.ae>
2022-12-08 09:10:32 +0300

    Find usages
     - add find usages contributor for functions

--- src/main/kotlin/com/wavesplatform/rideplugin/psi/mixin/RideVarNameMixin.kt
+++ src/main/kotlin/com/wavesplatform/rideplugin/psi/mixin/RideVarNameMixin.kt
@@ -4,7 +4,6 @@ import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiReference
 import com.wavesplatform.rideplugin.psi.impl.RideCompositeElementImpl
-import com.wavesplatform.rideplugin.resolve.RideFunctionReference
 import com.wavesplatform.rideplugin.resolve.RideVarReference
 
 open class RideVarNameMixin(node: ASTNode) : RideCompositeElementImpl(node) {
