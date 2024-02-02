e2e4df0da46b68bbccc298841ea6c7ee0f791889
Sergey Nazarov <phearnot@renee.ru>
2024-02-02 16:52:50 +0400

    Bumped dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,14 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.17.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.changelog") version "2.2.0"
     // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
+    id("org.jetbrains.qodana") version "2023.3.1"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 sourceSets["main"].java.srcDirs("src/main/gen")
@@ -46,12 +47,10 @@ changelog {
     groups.set(emptyList())
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
+// Configure Gradle Qodana Plugin - read more: https://www.jetbrains.com/help/qodana/qodana-gradle-plugin.html
 qodana {
     cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
+    resultsPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
 }
 
 tasks {
